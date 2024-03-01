f809fdd4bd85f9f1aba58d40c4699586d8675522
Helge Falch <helge@kubes.no>
2024-03-01 18:38:52 +0100

    Working dev containers

---
+++ .devcontainer/initdb.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE coffee;
+
+\c coffee;
+
+CREATE TABLE IF NOT EXISTS beans (
+    id SERIAL PRIMARY KEY,
+    country VARCHAR(255) NOT NULL,
+    region VARCHAR(255) NOT NULL,
+    producer VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    process VARCHAR(255) NOT NULL,
+    flavours VARCHAR(255) NOT NULL
+);
+
+INSERT INTO beans (country, region, producer, name, process, flavours) VALUES
+  ('Ethiopia', 'Yirgacheffe', 'Munin', 'Burtukaana Wote Konga', 'Natural', 'Round, Citric, With Stone fruit notes, and a good structure.'),
+  ('Ethiopia', 'Sidamo', 'Munin', 'Samii Bensa', 'Washed', 'Herbal, tropical, fruity acidity.');
