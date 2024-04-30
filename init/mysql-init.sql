1dca0237a3429ded8e17f4ffd56833d795019365
Helge Falch <helge@kubes.no>
2024-04-30 11:04:07 +0200

    Added more test variants

--- init/mysql-init.sql
+++ init/mysql-init.sql
@@ -15,4 +15,6 @@ CREATE TABLE IF NOT EXISTS beans (
 INSERT INTO beans (country, region, producer, name, process, flavours) VALUES
   ("Ethiopia","Yirgacheffe","Munin","Burtukaana Wote Konga","Natural","Round, Citric, With Stone fruit notes, and a good structure."),
   ("Ethiopia","Sidamo","Munin","Samii Bensa","Washed","Herbal, tropical, fruity acidity."),
-  ("Ethiopia", "Yirgacheffe", "Munin", "Burtukaana Danche","Natural", "Round, Sweety, Citric, with stone fruit notes");
+  ("Ethiopia", "Yirgacheffe", "Munin", "Burtukaana Danche","Natural", "Round, Sweety, Citric, with stone fruit notes"),
+  ("El Salvador", "Apaneca-Ilamatepec", "Jacobsen og Svart", "El Limo", "Anaerobisk Ferment", "Vinous, floral, tropical fruit and caramel"),
+  ("Ethiopia", "Shakisso, Guji", "Jacobsen og Svart", "Koromii", "Berry dried", "Strawberry, fruit, mandarin");
